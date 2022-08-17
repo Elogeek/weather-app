afb679f5e21b31924c474341918af3d39a79378a
elogeek <elodiechristin@gmail.com>
2022-08-17 12:20:33 +0200

    start weather app

--- App.js
+++ App.js
@@ -1,18 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import Constants from "expo-constants";
 import * as Location from "expo-location";
+import {useState} from "react";
 
 export default function App() {
 
   // récupération des datas user
+  const [location, setLocation] = useState(null);
+  React.useEffect(() => {
+    // récupération de la position où se trouve l'user
+    const getCoordinates = async () => {
 
+     const {status} = await Location.requestForegroundPermissionsAsync()
+      if(status !=="granted") {
+        return alert("Il faut accepter de partager votre position, sinon l'app ne fonctionnera pas correctement !")
+      }
+
+      const userLocation = await  Location.getCurrentPositionAsync();
+      setLocation(userLocation);
+    }
+    getCoordinates()
+
+  }, [])
+  // check location not null
+  if(!location) {
+    return (
+      <View style={styles.container}>
+        <Text> Location est null </Text>
+      </View>
+    );
+  }
   // faire une request vers le serveur (city, méteo du moment,prévisions)
+
   return (
     <View style={styles.container}>
-
-      <StatusBar style="auto" />
+      <Text> {location.coords.latitude}</Text>
     </View>
   );
 }
