307bb5dc379c59f7a7b2f3eadc841cf7afc5182a
elogeek <elodiechristin@gmail.com>
2022-08-17 13:32:39 +0200

    suite weather app

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import Constants from "expo-constants";
 import * as Location from "expo-location";
 import {useState} from "react";
+import * as axios from "axios";
+
+const API_URL = (lat,lon) => "https://api.openweathermap.org/data/2.5/forecast?" +
+    "lat={lat}" +
+    "&lon={lon}" +
+    "&appid={API_KEY}" +
+    "&lang=fr"
+    "&units=metric"
+;
 
 export default function App() {
 
+    const [error, setError] = useState();
+
   // récupération des datas user
-  const [location, setLocation] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
+
   React.useEffect(() => {
     // récupération de la position où se trouve l'user
     const getCoordinates = async () => {
@@ -17,25 +30,35 @@ export default function App() {
         return alert("Il faut accepter de partager votre position, sinon l'app ne fonctionnera pas correctement !")
       }
 
-      const userLocation = await  Location.getCurrentPositionAsync();
-      setLocation(userLocation);
+      const userLocation = await Location.getCurrentPositionAsync();
+      getWeather(userLocation);
     }
     getCoordinates()
-
   }, [])
-  // check location not null
-  if(!location) {
-    return (
-      <View style={styles.container}>
-        <Text> Location est null </Text>
-      </View>
-    );
-  }
+
   // faire une request vers le serveur (city, méteo du moment,prévisions)
+ const getWeather = async(location) => {
+      try {
+          const response = axios.get(API_URL(location.coords.latitude, location.coords.longitude))
+          setData(response.data);
+          setLoading(false);
+      }
+      catch(e){
+        console.log("Error :" + setError(error))
+      }
+ }
+
+ if(loading) {
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator/>
+        </View>
+    );
+ }
 
   return (
     <View style={styles.container}>
-      <Text> {location.coords.latitude}</Text>
+      <Text> {data?.city?.name}</Text>
     </View>
   );
 }
@@ -43,7 +66,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ecf0f1',
     paddingTop: Constants.statusBarHeight,
     alignItems: 'center',
     justifyContent: 'center',
