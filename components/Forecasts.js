cfa4897b1ca994e21a709c8dd30e67d943cadfe6
elogeek <elodiechristin@gmail.com>
2022-08-19 11:41:58 +0200

    suite weather app

--- components/Forecasts.js
+++ components/Forecasts.js
@@ -1,20 +1,44 @@
-import React, {useEffect} from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React , {useEffect, useState} from "react";
+import {StyleSheet, View, Text, ScrollView} from "react-native";
+import {format} from "date-fns";
+import Weather from "./CurrentWeather";
 
-export default function Forecasts({data}) {
+export default function Forecasts({data}){
+    const [forecasts, setForecasts] = useState([]);
 
     useEffect(() => {
+        const forecastsData = data.list.map(f => {
+            const dt = new Date(f.dt * 1000);
+            return({
+                date: dt,
+                hour: dt.getHours(),
+                temp: Math.round(f.main.temp),
+                icon: f.weather[0].icon,
+                name: format(dt, "EEEE")
+            });
+        });
+        setForecasts(forecastsData);
+    }, [data]);
 
-    }, [data])
-    return (
-        <View>
-            <Text> les </Text>
-        </View>
-    )
+    return(
+        <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={styles.scroll}
+        >
+            {forecasts.map(f => (
+                <View>
+                    <Text>{f.name}</Text>
+                    <Weather forecast={f} />
+                </View>
+            ))}
+        </ScrollView>
+    );
 }
 
-const style = StyleSheet.create({
-
+const styles = StyleSheet.create({
+    scroll: {
+        width: "100%",
+        height: "35%"
     }
-
-)
+});
